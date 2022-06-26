4efcc1ee66a18905f6c65fd3e94801dc03fd9d49
Mustafa Turkoz <mturkoz93@gmail.com>
2022-06-26 18:33:38 +0300

    feature: added table rows

--- sayfa/film_listesi/FilmListesi.py
+++ sayfa/film_listesi/FilmListesi.py
@@ -46,7 +46,7 @@ class FilmListesi:
 
     def sayfa_olustur(self):
         self.add_title_row()
-        # self.tablo_olustur()
+        self.tablo_olustur()
 
     def add_title_row(self):
         for j, sutun in enumerate(FilmListesi.sutunlar):
@@ -76,4 +76,46 @@ class FilmListesi:
                     lbl.grid(row=1, column=j, sticky='we', padx=(0, 1))
 
 
+    def tablo_olustur(self):
+        for i, film in enumerate(self.filmler):
+            for j, key in enumerate(FilmListesi.sutunlar):
+                name = 'table_row' + str(i) + str(j) + '_' + film['imdbID']
+                if j == 0:
+                    # resim bastır
+                    self.render_image(film, i, j, name)
+                else:
+                    # yazı bastır
+                    self.write_label(film, i, j, key, name)
 
+    def render_image(self, film, i, j, name):
+        try:
+            # resim var mı
+            load = Image.open('images/posters_small/' + film['imdbID'] + '.jpg')
+        except:
+            # yoksa no_image'ı al
+            load = Image.open('images/posters_small/no_image.jpg')
+        finally:
+            render = ImageTk.PhotoImage(load)
+            img_label = tk.Label(self.frame, name=name, image=render, bg='orange')
+            img_label.image = render
+            img_label.grid(row=i + 2, column=j, padx=(7, 0), sticky='we')
+
+    def write_label(self, film, i, j, key, name):
+        lbl = tk.Label(self.frame, name=name, text=str(film[key]), height=4, fg='black', font=('Arial', 10, 'bold'),
+            cursor='hand2')
+        
+        if key == 'Id':
+            lbl.configure(width=4)
+        elif key == 'Title':
+            lbl.configure(width=54, anchor='w') # sola yasla
+        elif key == 'Year':
+            lbl.configure(width=8)
+        elif key == 'imdbRating':
+            lbl.configure(width=8)
+        elif key == 'imdbVotes':
+            lbl.configure(width=12)
+        
+        if key == 'imdbVotes':
+            lbl.grid(row=i + 2, column=j, sticky='we', padx=(0, 10))
+        else:
+            lbl.grid(row=i + 2, column=j, sticky='we', padx=(0, 1))
