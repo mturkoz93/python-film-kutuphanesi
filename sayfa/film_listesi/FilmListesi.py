f2553953ab91b24f1f7da689ea42419909d31e55
Mustafa Turkoz <mturkoz93@gmail.com>
2022-06-26 17:11:25 +0300

    feature: film list header

--- sayfa/film_listesi/FilmListesi.py
+++ sayfa/film_listesi/FilmListesi.py
@@ -25,6 +25,8 @@ class FilmListesi:
 
     def frame_ekle(self):
         self.frame_basligi('Film Listesi')
+        self.csv_oku()
+        self.sayfa_olustur()
         self.frame.pack(side=self.side, fill=tk.BOTH, expand=True)
 
     def frame_basligi(self, baslik):
@@ -34,5 +36,44 @@ class FilmListesi:
         )
 
         bsl.grid(row=0, column=0, columnspan=8, padx=1, pady=(0,8), sticky='we')
+
+    def csv_oku(self):
+        film_oku = 'data/imdb_top_250.csv'
+        with open(film_oku, 'r') as dosya:
+            film_dict = csv.DictReader(dosya, delimiter=';')
+            for film in film_dict:
+                self.filmler.append(film)
+
+    def sayfa_olustur(self):
+        self.add_title_row()
+        # self.tablo_olustur()
+
+    def add_title_row(self):
+        for j, sutun in enumerate(FilmListesi.sutunlar):
+            if sutun != 'imdbID':
+                lbl = tk.Label(
+                    self.frame, text=str(sutun),
+                    width=54,
+                    height=2,
+                    bg=COLORS.SIYAH,
+                    fg='white',
+                    font=('Arial', 10, 'bold')
+                )
+                if sutun == 'Id':
+                    lbl.configure(text='#', width=4)
+                elif sutun == 'Title':
+                    lbl.configure(text='Film Adı')
+                elif sutun == 'Year':
+                    lbl.configure(text='Yıl', width=8)
+                elif sutun == 'imdbRating':
+                    lbl.configure(text='Puanı', width=8)
+                elif sutun == 'imdbVotes':
+                    lbl.configure(text='Oy Sayısı', width=12)
+
+                if sutun == 'imdbVotes':
+                    lbl.grid(row=1, column=j, sticky='we', padx=(0, 10))
+                else:
+                    lbl.grid(row=1, column=j, sticky='we', padx=(0, 1))
+
 
 
